01f4b78bbf901870ba3833aafcf79938a820c02f
dhilipsiva <dhilipsiva@gmail.com>
2016-07-08 22:01:32 +0530

    Add a runscript

---
+++ run.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# run.sh
+# Copyright (C) 2016 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+DATABASE_URL="sqlite3:///db.sqlite3" ./manage.py runserver 9999
